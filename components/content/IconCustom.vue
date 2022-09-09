5ab7df82f9c9c6027d253c21230da2b0e33649a6
Ben <3126319718@qq.com>
2022-09-09 22:06:54 +0800

    add more custom options for IconCustom component.

--- components/content/IconCustom.vue
+++ components/content/IconCustom.vue
@@ -4,13 +4,15 @@ import type { Ref } from 'vue'
 import type { IconifyIcon } from '@iconify/vue'
 import { Icon as Iconify, loadIcon } from '@iconify/vue'
 
-const props = defineProps({
-  name: {
-    type: String,
-    required: true
-  }
-})
+const props = defineProps<{
+  name: string;
+  iconClass?: string;
+  colorStyle?: string;
+  widthStyle?: string;
+  heightStyle?: string;
+}>()
 
+// icon
 const icon: Ref<IconifyIcon | null> = ref(null)
 
 icon.value = await loadIcon(props.name).catch(_ => null)
@@ -18,9 +20,30 @@ icon.value = await loadIcon(props.name).catch(_ => null)
 watch(() => props.name, async () => {
   icon.value = await loadIcon(props.name).catch(_ => null)
 })
+
+// class and style value
+let iconStyle = ''
+
+if (props.colorStyle) {
+  iconStyle += ' ' + `color: ${props.colorStyle}`
+}
+
+if (props.widthStyle) {
+  iconStyle += ' ' + `width: ${props.widthStyle}`
+}
+
+if (props.heightStyle) {
+  iconStyle += ' ' + `width: ${props.heightStyle}`
+}
 </script>
 
 <template>
-  <Iconify v-if="icon" :icon="icon" class="inline-block" />
+  <Iconify
+    v-if="icon"
+    :icon="icon"
+    class="inline-block"
+    :class="props.iconClass"
+    :style="iconStyle"
+  />
   <span v-else>{{ name }}</span>
 </template>
