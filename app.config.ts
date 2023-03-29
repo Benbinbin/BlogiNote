384e77c0ac0cf1cc18837d5cbfbbd0a9d684f13b
Benbinbin <benthomsonbin@gmail.com>
2023-03-29 17:52:13 +0800

    :bug: fix bug for error handling the asset files link

--- app.config.ts
+++ app.config.ts
@@ -32,6 +32,7 @@ export default defineAppConfig({
         '/subscribe/',
         '/list/'
       ]
-    }
+    },
+    assetTypes: []
   }
 })
--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -1,18 +1,34 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   href: {
     type: String,
     default: ''
   },
-  blank: {
-    type: Boolean,
-    default: false
+  target: {
+    type: String,
+    default: undefined,
+    required: false
   }
 })
+
+const isRelative = /^(\.\/|\.\.\/|\/|@\/|~\/)/.test(props.href)
+const isAssetType = ref(false)
+
+if (isRelative) {
+  const appConfig = useAppConfig()
+  const extensionArr = appConfig.theme.assetTypes
+
+  const result = props.href.match(/\.(\w+)$/)
+
+  if (result) {
+    const ext = result[1]
+    isAssetType.value = extensionArr.includes(ext)
+  }
+}
 </script>
 
 <template>
-  <NuxtLink :href="href">
+  <NuxtLink :href="href" :target="isRelative && isAssetType ? '_blank' : target" :external="isRelative && isAssetType">
     <slot />
   </NuxtLink>
 </template>
